82342c070dc35b1f8c0f9a67af86fc1146665702
Jonas Santoso <35079323+santosoj@users.noreply.github.com>
2024-11-11 16:43:58 +1100

    pass countrySet as parameter to getPlaceAutoComplete

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ export type AddressResult = {
 export async function getAutoCompleteDetails(address: any): Promise<[AddressResult]> {
     const apiKey = process.env.TOMTOM_API_KEY;
     // get autocomplete results
-    const res = getPlaceAutocomplete(apiKey, address).then(async (autocompleteResults) => {
+    const res = getPlaceAutocomplete(apiKey, address, { countrySet: 'AU' }).then(async (autocompleteResults) => {
         return autocompleteResults.map((result) => ({
             placeId: result.placeId,
             streetNumber: result.address.streetNumber,
--- src/maps-api.ts
+++ src/maps-api.ts
@@ -2,12 +2,12 @@ import axios from 'axios'
 
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
-export async function getPlaceAutocomplete(key: string, address: string): Promise<[any]> {
+export async function getPlaceAutocomplete(key: string, address: string, additionalParams: { [k: string]: string }): Promise<[any]> {
     const autocomplete = await axios.get(`https://api.tomtom.com/search/2/search/${address}.json'`, {
         params: {
             key,
             limit: 100,
-            countrySet: 'AU',
+            ...additionalParams
         }
     });
     return autocomplete.data.results.map((result) => {
