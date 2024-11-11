0b5d2f09e5d687f44137bd94ba7e3d1a4cbb46dd
Jonas Santoso <35079323+santosoj@users.noreply.github.com>
2024-11-11 16:39:55 +1100

    move AddressResult mapping to getAutoCompleteDetails

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import { getPlaceAutocomplete } from './maps-api'
-import type { AddressResult } from './maps-api'
+
+export type AddressResult = {
+    placeId: string,
+    streetNumber: string | undefined,
+    streetName: string,
+    countryCode: string,
+    country: string,
+    freeformAddress: string,
+    municipality: string,
+    postalCode: string,
+}
 
 export async function getAutoCompleteDetails(address: any): Promise<[AddressResult]> {
     const apiKey = process.env.TOMTOM_API_KEY;
     // get autocomplete results
     const res = getPlaceAutocomplete(apiKey, address).then(async (autocompleteResults) => {
-        return autocompleteResults
+        return autocompleteResults.map((result) => ({
+            placeId: result.placeId,
+            streetNumber: result.address.streetNumber,
+            streetName: result.address.streetName,
+            countryCode: result.address.countryCode,
+            country: result.address.country,
+            freeformAddress: result.address.freeformAddress,
+            municipality: result.address.municipality,
+            postalCode: result.address.postalCode,
+        }))
     })
     return res
 }
